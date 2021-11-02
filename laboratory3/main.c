a3966aaa2b4b24390c20e7c044ce6b4bbd305ae8
Mikhail Fomenko <mikhail@Mikhails-iMac.local>
2021-11-02 13:08:46 +0300

    small fixes for laboratory3

--- laboratory3/main.c
+++ laboratory3/main.c
@@ -14,49 +14,51 @@ int main() {
 
     setlocale(LC_ALL, "RUS");
 
-    printf("Введите путь до файла: ");
+    char *files[] = {"./tests/genya.txt", "./tests/barselona.txt", "./tests/sevastopol.txt", "./tests/soshi.txt", "./tests/valencia.txt"};
+    int N_FILES = 5;
+    int i;
+    for (i = 0; i < N_FILES; i++) {
+        char *filename = *(files + i);
+        if((file = fopen(filename, "r")) == NULL) {
+            printf("Файл \"%s\" не найден!", filename);
+            return 0;
+        }
 
-    char *filename = malloc(100);
-    fgets(filename, 100, stdin);
-    filename[strlen(filename) - 1] = '\0';
+        printf("Ввод из файла: %s\n", filename);
 
-    if((file = fopen(filename, "r")) == NULL) {
-        printf("Файл \"%s\" не найден!", filename);
-        return 0;
+        // Массив структур кораблей
+        struct position ship[MAX_SIZE];
+        // Местоположение запретной зоны
+        struct fig triangle;
+        // Координаты порта назначения
+        struct position port;
+        // Матрица угловых коэффициентов направлений судов на порт и вершины треугольника запретной зоны.
+        float slopes[MAX_SIZE][4] = {};
+        // В результате решения задачи, получим массив одномерный массив, в котором значение i-го элемента равно 1, если i-е судно пересекает запрещенную зону и 0 в противном случае.
+        int  danger[MAX_SIZE] = {};
+
+        // Вводим кол-во кораблей
+        int N;
+        fscanf(file, "%d", &N);
+
+        // Вводим местоположение порта
+        port = input_port();
+
+        // Вводим информацию о кораблях
+        input_ship(N, ship);
+
+        // Вводим запрещенную зону
+        triangle = input_triangle();
+
+        // Находим угловые коэффициенты
+        matrix_of_slopes(N, ship, triangle, port, slopes);
+
+        // Информация об опасности
+        danger_identification(N, slopes, danger);
+
+        // Выдаем на печать таблицу результатов
+        danger_signal(N, danger);
     }
-
-    // Массив структур кораблей
-    struct position ship[MAX_SIZE];
-    // Местоположение запретной зоны
-    struct fig triangle;
-    // Координаты порта назначения
-    struct position port;
-    // Матрица угловых коэффициентов направлений судов на порт и вершины треугольника запретной зоны.
-    float slopes[MAX_SIZE][4] = {};
-    // В результате решения задачи, получим массив одномерный массив, в котором значение i-го элемента равно 1, если i-е судно пересекает запрещенную зону и 0 в противном случае.
-    int  danger[MAX_SIZE] = {};
-
-    // Вводим кол-во кораблей
-    int N;
-    fscanf(file, "%d", &N);
-
-    // Вводим местоположение порта
-    port = input_port();
-
-    // Вводим информацию о кораблях
-    input_ship(N, ship);
-
-    // Вводим запрещенную зону
-    triangle = input_triangle();
-
-    // Находим угловые коэффициенты
-    matrix_of_slopes(N, ship, triangle, port, slopes);
-
-    // Информация об опасности
-    danger_identification(N, slopes, danger);
-
-    // Выдаем на печать таблицу результатов
-    danger_signal(N, danger);
 
     return 0;
 }
